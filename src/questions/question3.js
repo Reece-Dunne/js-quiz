ed5a5db9a3041bc8c6fb213dbe0a1dd037780dd6
Reece Dunne <reece.dunne@dpdgroup.co.uk>
2021-02-18 16:31:39 +0000

    Question 3 Commit

--- src/questions/question3.js
+++ src/questions/question3.js
@@ -23,6 +23,16 @@ exports.questionThree = () => {
 }
 
 const answerThree = (names) => {
-  //code here
+  let str = '';
+  if (names.length !== 0) {
+    let last = names.pop();
+    str = names.map( (val, i, arr,) => {
+      if (i !== arr[arr.length -1]) {
+        return val.name
+      }
+    }).join(', ')
+    str += str !== '' ? ' & ' + last.name : last.name;
+  }
   
+  return str;
 }
